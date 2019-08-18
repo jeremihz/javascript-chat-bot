dc090432e47d64bd1128cffd774e129c59f71fdb
jeremihz <mihjere68@gmail.com>
2019-08-18 21:07:29 +0300

    simple chat bot javascript

---
+++ getresponse.php
@@ -0,0 +1,28 @@
+<?php 
+	$responses['what is your name'] = "My name is Vincent.";
+	// echo "Hello world";
+	$responses['tell me about yourself'] = "I am a chatbot. I'm still learning a lot of things so please forgive me if I can't answer you in some cases.";
+	
+	$responses["i'm fine"] = "Good. I'm happy about that.";
+	$responses["can i ask you anything?"] = "sure go ahead";
+	
+	$q = $_GET["q"];
+
+	$response = "";
+
+	if ($q != "") {
+		# code...
+		$q = strtolower($q);
+		foreach ($responses as $r => $value) {
+			# code...
+			if (strpos($r, $q) !== false) {
+				# code...
+				$response = $value;
+			}
+
+			
+		}
+	}
+	$noresponse = "Sorry I'm still learning. Hence my responses are limited. Ask something else.";
+	echo $response === "" ? $noresponse : $response;
+?>
